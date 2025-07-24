be7f35db3b5a2420917722dbb4c731b941ba3c43
okolacki <129507504+okolacki@users.noreply.github.com>
2025-07-24 11:58:04 +0200

    header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function Header(){
-    const [isDarkMode, setIsDarkMode] = useState(false)
+function Header() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     useEffect(() => {
         const darkMode = localStorage.getItem('dark-mode') === 'true';
         setIsDarkMode(darkMode);
-        if (darkMode){
-            document.body.classNameList.add('dark-mode');
+        if (darkMode) {
+            document.body.classList.add('dark-mode');
         }
     }, []);
 
@@ -16,26 +16,36 @@ function Header(){
         const newMode = !isDarkMode;
         setIsDarkMode(newMode);
         localStorage.setItem('dark-mode', newMode);
-        document.body.classNameList.toggle('dark-mode');
+        document.body.classList.toggle('dark-mode');
     };
-    return(
+
+    return (
         <header>
-			<div className="container">
-				<h1>🏟️ Spot The Sport</h1>
-				<nav>
-					<ul>
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/gallery">Gallery</Link></li>
-						<li><Link to="/">Credits</Link></li>
-						<li><Link to="/">Location</Link></li>
-						<li><Link to="/">DOG API</Link></li>
+            <div className="container">
+                <h1>🏟️ Spot The Sport</h1>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/gallery">Gallery</Link></li>
+                        <li><Link to="/">Credits</Link></li>
+                        <li><Link to="/">Location</Link></li>
+                        <li><Link to="/">DOG API</Link></li>
                         <li className="nav-item">Dark mode:</li>
-                        <li className="nav-item"><label className="switch"><input type="checkbox" id="theme-toggle" checked={isDarkMode} onChange={handleToggle}/><span className="slider"></span></label></li>
-					</ul>
-				</nav>
-			</div>
-		</header>
-    )
+                        <li className="nav-item">
+                            <label className="switch">
+                                <input
+                                    type="checkbox"
+                                    checked={isDarkMode}
+                                    onChange={handleToggle}
+                                />
+                                <span className="slider"></span>
+                            </label>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
 }
 
-export default Header
+export default Header;
