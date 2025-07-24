35022ddebfe375ddddd4e0b41ed900965adb8904
remuwson <remigiuszablowski@gmail.com>
2025-07-24 11:50:55 +0200

    New style of description

--- src/App.jsx
+++ src/App.jsx
@@ -25,58 +25,67 @@ function App() {
       <main class="credits-main">
         <div class="credits-row">
           <div class="credits-container">
-            {photo === "Photo" && (
-              <>
+            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div class="flip-front">
                 <img src='images/Remek.jpg' alt='Remy' />
                 <h3>Remek Szabłowski</h3>
-              </>
-            )}
-            {
-              photo === "Description" &&
-              <p>Amazing Personality</p>
-            }
+              </div>
+              <div class="flip-back">
+                <p>Amazing Personality</p>
+              </div>
+            </div>
           </div>
           <div class="credits-container">
-            {photo === "Photo" && <>
-              <img src="images/Max.jpg" alt="Max" />
-              <h3>Max Wyżykowski</h3>
-            </>}
-            {photo === "Description" &&
-              <p>Best Rizz</p>
-            }
+            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div class="flip-front">
+                <img src="images/Max.jpg" alt="Max" />
+                <h3>Max Wyżykowski</h3>
+              </div>
+              <div class="flip-back">
+                <p>Best Rizz</p>
+              </div>
+            </div>
           </div>
           <div class="credits-container">
-            {photo === "Photo" && <>
-              <img src="images/Alex.jpg" alt="Alex" />
-              <h3>Alex Łysakowski</h3>
-            </>}
-            {photo === "Description" &&
-              <p>Just Cool Guy</p>
-            }
+            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div class="flip-front">
+                <img src="images/Alex.jpg" alt="Alex" />
+                <h3>Alex Łysakowski</h3>
+              </div>
+              <div class="flip-back">
+                <p>Just Cool Guy</p>
+              </div>
+            </div>
           </div>
         </div>
         <div class="credits-row2">
           <div class="credits-container">
-            {photo === "Photo" && <>
-              <img src="images/Oli.png" alt="Oliver" />
-              <h3>Oliver Kołacki</h3>
-            </>}
-            {photo === "Description" &&
-              <p>Awesome Person</p>
-            }
+            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div class="flip-front">
+                <img src="images/Oli.png" alt="Oliver" />
+                <h3>Oliver Kołacki</h3>
+              </div>
+              <div class="flip-back">
+                <p>Awesome Person</p>
+              </div>
+            </div>
           </div>
+
           <div>
             <button onClick={() => setPhoto("Photo")}>Photo</button>
             <button onClick={() => setPhoto("Description")}>Description</button>
           </div>
           <div class="credits-container">
-            {photo === "Photo" && <>
-              <img src="images/Ostry.jpg" alt="Sharpee" />
-              <h3>Mateusz Ostrowski</h3>
-            </>}
-            {photo === "Description" &&
-              <p>The Best One</p>
-            }
+            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div class="flip-front">
+                <img src="images/Ostry.jpg" alt="Sharpee" />
+                <h3>Mateusz Ostrowski</h3>
+              </div>
+              <div class="flip-back">
+                <p>The Best One</p>
+              </div>
+
+            </div>
           </div>
         </div>
       </main>
