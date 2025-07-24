29e771ce50ef5316f5194b91e72b47634c3a8bdd
okolacki <129507504+okolacki@users.noreply.github.com>
2025-07-24 11:50:06 +0200

    header

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './index.jsx'
-import Footer from './footer.jsx'
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <App />
     <Footer />
   </StrictMode>,
