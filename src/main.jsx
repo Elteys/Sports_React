d0dd88408f406e248d49540728073989e0ce98e4
mrstahuu <126872857+mrstahuu@users.noreply.github.com>
2025-07-24 11:48:28 +0200

    Dodalem header

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import Header from './header.jsx'
 import App from './index.jsx'
 import Footer from './footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <App />
     <Footer />
   </StrictMode>,
