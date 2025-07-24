3539636c633785dff1ed2712b4f6585f14fa9296
mrstahuu <stanislaw.maik@gmail.com>
2025-07-24 11:57:17 +0200

    Mjr chgs

--- src/locations.jsx
+++ src/locations.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './locations_App.jsx'
+import { useEffect, useState } from 'react'
+import './locations.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function Locations() {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.maventplan.pl/sports_react/api")
+      .then((res) => res.json())
+      .then((data) => setSpots(data))
+      .catch((err) => console.error("Błąd ładowania danych:", err));
+  }, []);
+
+  return (
+    <div className="fitness-container">
+      {spots.map((spot, index) => (
+        <div className="img-text" key={index}>
+          <div className="left">
+            {spot.map_embed ? (
+              <iframe
+                className="map"
+                src={spot.map_embed}
+                width="600"
+                height="450"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title={`map-${index}`}
+              ></iframe>
+            ) : spot.image ? (
+              <img
+                src={spot.image}
+                alt={spot.name || `Obraz ${index}`}
+                width="600"
+                height="450"
+                style={{ objectFit: "cover", borderRadius: "12px" }}
+              />
+            ) : null}
+          </div>
+          <div className="right">
+            <div className="text">
+              <h3>{spot.name}</h3>
+              {spot.description && <p>{spot.description}</p>}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+export default Locations
