6502a439b3c34c6e3075f6626a06a9c9240ce81a
mrstahuu <126872857+mrstahuu@users.noreply.github.com>
2025-07-24 11:57:53 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Header from './Header.jsx'
+import Header from './header.jsx'
 import App from './index.jsx'
-import Footer from './Footer.jsx'
+import Footer from './footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
