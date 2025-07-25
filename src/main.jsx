1a1c3a6903b929f42930c4cc0d3b108c53ddb0ec
Elteys <ntcc.elteys@gmail.com>
2025-07-25 11:24:59 +0200

    mala zmiana

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Header from './Header.jsx'
-import App from './index.jsx'
+import App from './App.jsx'
 import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <App />
-    <Footer />
+   <Footer />
   </StrictMode>,
 )
