e52f831b3433628e12d792828dad264d55d149b5
okolacki <129507504+okolacki@users.noreply.github.com>
2025-07-28 09:47:37 +0200

    class naprwione button credits

--- src/Credits.jsx
+++ src/Credits.jsx
@@ -14,15 +14,15 @@ function Credits() {
   /*  const toggle = document.getElementById('theme-toggle');
   
     if (localStorage.getItem('dark-mode') === 'true') {
-      document.body.classList.add('dark-mode');
+      document.body.classNameList.add('dark-mode');
       toggle.checked = true;
     }
   
     toggle.addEventListener('change', () => {
-      document.body.classList.toggle('dark-mode');
+      document.body.classNameList.toggle('dark-mode');
       localStorage.setItem(
         'dark-mode',
-        document.body.classList.contains('dark-mode')
+        document.body.classNameList.contains('dark-mode')
       );
     });
   */
@@ -30,66 +30,66 @@ function Credits() {
   return (
     <>
     <Header />
-      <main class="credits-main">
-        <div class="credits-row">
-          <div class="credits-container">
-            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
-              <div class="flip-front">
+      <main className="credits-main">
+        <div className="credits-row">
+          <div className="credits-container">
+            <div className={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div className="flip-front">
                 <img src={remekPhoto} alt='Remy' />
                 <h3>Remek Szabłowski</h3>
               </div>
-              <div class="flip-back">
+              <div className="flip-back">
                 <p>Amazing Personality</p>
               </div>
             </div>
           </div>
-          <div class="credits-container">
-            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
-              <div class="flip-front">
+          <div className="credits-container">
+            <div className={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div className="flip-front">
                 <img src={maxPhoto} alt="Max" />
                 <h3>Max Wyżykowski</h3>
               </div>
-              <div class="flip-back">
+              <div className="flip-back">
                 <p>Best Rizz</p>
               </div>
             </div>
           </div>
-          <div class="credits-container">
-            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
-              <div class="flip-front">
+          <div className="credits-container">
+            <div className={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div className="flip-front">
                 <img src={alexPhoto} alt="Alex" />
                 <h3>Alex Łysakowski</h3>
               </div>
-              <div class="flip-back">
+              <div className="flip-back">
                 <p>Just Cool Guy</p>
               </div>
             </div>
           </div>
         </div>
-        <div class="credits-row2">
-          <div class="credits-container">
-            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
-              <div class="flip-front">
+        <div className="credits-row2">
+          <div className="credits-container">
+            <div className={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div className="flip-front">
                 <img src={oliPhoto} alt="Oliver" />
                 <h3>Oliver Kołacki</h3>
               </div>
-              <div class="flip-back">
+              <div className="flip-back">
                 <p>Awesome Person</p>
               </div>
             </div>
           </div>
 
           <div>
-            <button onClick={() => setPhoto("Photo")}>Photo</button>
-            <button onClick={() => setPhoto("Description")}>Description</button>
+            <button className="cta-button" onClick={() => setPhoto("Photo")}>Photo</button>
+            <button className="cta-button" onClick={() => setPhoto("Description")}>Description</button>
           </div>
-          <div class="credits-container">
-            <div class={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
-              <div class="flip-front">
+          <div className="credits-container">
+            <div className={`flip-inner ${photo === "Description" ? "flipped" : ""}`}>
+              <div className="flip-front">
                 <img src={ostryPhoto} alt="Sharpee" />
                 <h3>Mateusz Ostrowski</h3>
               </div>
-              <div class="flip-back">
+              <div className="flip-back">
                 <p>The Best One</p>
               </div>
 
