b311561d2a0e359fb38ad9303cd0ecb231f80428
Elteys <ntcc.elteys@gmail.com>
2025-07-28 09:42:46 +0200

    naprawione

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Sports_React">
+    <BrowserRouter basename="/Sports_React/">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/Credits" element={<Credits />} />
